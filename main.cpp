72e99ee498940c4e793817c9fdb0101bafe2a326
ahmedelagezy0@gmail.com <ahmedelagezy0@gmail.com>
2023-01-06 17:01:27 +0200

    comment

--- main.cpp
+++ main.cpp
@@ -3,15 +3,7 @@
 
 int main()
 {
-	/*
-	GUI* g=new GUI();
-	
-	
-	window* w=g->CreateWind(200, 300, 10, 10);
-	int x, y;
-	w->WaitMouseClick(x, y);
-	*/
-	
+
 	ApplicationManager AppManager;
 	AppManager.Run();		
 	return 0;
